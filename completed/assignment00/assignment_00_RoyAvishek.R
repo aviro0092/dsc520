6e935b05ac0285e15258be00f077e67390ed5fe9
aviro0092 <aviro092@gmail.com>
2020-09-05 17:47:51 -0500

    Assignment week 1
    Assignment week 1

--- completed/assignment00/assignment_00_RoyAvishek.R
+++ completed/assignment00/assignment_00_RoyAvishek.R
@@ -1,22 +1,26 @@
 # Assignment: ASSIGNMENT 0
-# Name: Doe, John
-# Date: 2020-01-01
+# Name: Roy, Avishek
+# Date: 2010-02-14
 
 # Basics
 
 ## Add 8 and 5
+
 8 + 5
 
 ## Subtract 6 from 22
+
 22 - 6
 
 ## Multiply 6 by 7
-6 * 7
 
+6 * 7
 ## Add 4 to 6 and divide the result by 2
-(4 + 6) / 2
+
+(4+6) / 2
 
 ## Compute 5 modulo 2
+
 5 %% 2
 
 ## Assign the value 82 to the variable x
@@ -27,6 +31,7 @@ x
 ## Assign the value 41 to the variable y
 ## Print y
 y <- 41
+y
 
 ## Assign the output of x + y to the variable z
 ## Print z
@@ -38,15 +43,16 @@ z
 class_name <- "DSC520"
 class_name
 
+
 ## Assign the string value of TRUE to the variable is_good
 ## Print the value of is_good
 is_good <- TRUE
 is_good
 
-## Check the class of the variable is_good using the class() function
+## Check the class of the variable is_good using the `class()` function
 class(is_good)
 
-## Check the class of the variable z using the class() function
+## Check the class of the variable z using the `class()` function
 class(z)
 
 ## Check the class of the variable class_name using the class() function
